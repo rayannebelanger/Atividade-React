cea59d24e3d1f016ebefa6cb7dbd4f7451554b72
Rayanne Belanger <135059362+rayannebelanger@users.noreply.github.com>
2024-08-13 00:06:34 -0300

    initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import ImgComponent from "./components/imgComponent";
+
+function App() {
+  return (
+    <div>
+      <h1>Hello World</h1>
+      <ImgComponent  /> 
+      <hr />
+      <ImgComponent  /> 
+      <hr />
+      <ImgComponent  /> 
+      <hr />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/imgComponent.jsx
@@ -0,0 +1,17 @@
+// src/components/MeuComponente.js
+import React from 'react';
+
+function ImgComponent() {
+  return (
+    <div>
+      <h1>Olá, mundo!</h1>
+      <p>Este é um componente React.</p>
+      <img
+        src="https://i.imgur.com/MK3eW3Am.jpg"
+        alt="Katherine Johnson"
+      />
+    </div>
+  );
+}
+
+export default ImgComponent;
